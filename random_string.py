3b5db77811027c33b1ddd5fda8554f019d5d863e
Cody L. Kochmann <kochmanncody@gmail.com>
2015-05-15 16:35:39 -0700

    added random_string generator in python

---
+++ random_string.py
@@ -0,0 +1,12 @@
+def random_string(length=32,upper=True,lower=True,digits=True):
+    # Generates a random string with selectable attributes
+    # By: Cody Kochmann
+    from random import choice
+    chars = ""
+    if upper:
+        chars+="QWERTYUIOPASDFGHJKLZXCVBNM"
+    if lower:
+        chars+="qwertyuiopasdfghjklzxcvbnm"
+    if digits:
+        chars+="1234567890"
+    return(''.join(choice(chars) for _ in range(length)))
