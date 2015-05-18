37ad7373a5271dc3a5cd39f2843bf24239234089
Cody L. Kochmann <kochmanncody@gmail.com>
2015-05-17 23:38:08 -0700

    improved random letter generator memory usage.

--- random_string.py
+++ random_string.py
@@ -2,11 +2,12 @@ def random_string(length=32,upper=True,lower=True,digits=True):
     # Generates a random string with selectable characters
     # By: Cody Kochmann
     from random import choice
+    letters = "qwertyuiopasdfghjklzxcvbnm"
     chars = ""
     if upper:
-        chars+="QWERTYUIOPASDFGHJKLZXCVBNM"
+        chars+=letters.upper()
     if lower:
-        chars+="qwertyuiopasdfghjklzxcvbnm"
+        chars+=letters
     if digits:
         chars+="1234567890"
     return(''.join(choice(chars) for _ in range(length)))
