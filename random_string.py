55c1062649f274f512c90cdb1f2f962b8fbb11f1
Cody L. Kochmann <kochmanncody@gmail.com>
2015-05-20 05:46:05 -0700

    went back to more optimized method with raw characters

--- random_string.py
+++ random_string.py
@@ -2,12 +2,11 @@ def random_string(length=32,upper=True,lower=True,digits=True):
     # Generates a random string with selectable characters
     # By: Cody Kochmann
     from random import choice
-    letters = "qwertyuiopasdfghjklzxcvbnm"
     chars = ""
     if upper:
-        chars+=letters.upper()
+        chars+="QWERTYUIOPASDFGHJKLZXCVBNM"
     if lower:
-        chars+=letters
+        chars+="qwertyuiopasdfghjklzxcvbnm"
     if digits:
         chars+="1234567890"
     return(''.join(choice(chars) for _ in range(length)))
